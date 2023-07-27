49768f856bed44ce839e34ddc6a289cbb7ff8616
Paul Vanderbeken <paul.vand.fdd@gmail.com>
2023-07-27 15:09:57 +0200

    Add the Person entity

--- Stereograph.TechnicalTest.Api/Entities/ApplicationDbContext.cs
+++ Stereograph.TechnicalTest.Api/Entities/ApplicationDbContext.cs
@@ -4,6 +4,8 @@ namespace Stereograph.TechnicalTest.Api.Models;
 
 public class ApplicationDbContext : DbContext
 {
+    public DbSet<Person> Persons { get; set; }
+
     public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
     {
     }
