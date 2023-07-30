e81456303a7a5d598f96a742d0e3ec3f6665e3a7
Paul Vanderbeken <paul.vand.fdd@gmail.com>
2023-07-30 23:08:53 +0200

    Add a migration for the Following table creation

--- Stereograph.TechnicalTest.Api/Migrations/20230730200845_FollowingsTableCreation.Designer.cs
+++ Stereograph.TechnicalTest.Api/Migrations/20230730200845_FollowingsTableCreation.Designer.cs
@@ -2,6 +2,7 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Stereograph.TechnicalTest.Api.Models;
 
@@ -10,9 +11,11 @@ using Stereograph.TechnicalTest.Api.Models;
 namespace Stereograph.TechnicalTest.Api.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20230730200845_FollowingsTableCreation")]
+    partial class FollowingsTableCreation
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder.HasAnnotation("ProductVersion", "7.0.5");
---
+++ Stereograph.TechnicalTest.Api/Migrations/20230730200845_FollowingsTableCreation.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Stereograph.TechnicalTest.Api.Migrations
+{
+    /// <inheritdoc />
+    public partial class FollowingsTableCreation : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Followings",
+                columns: table => new
+                {
+                    FollowerId = table.Column<int>(type: "INTEGER", nullable: false),
+                    FollowedPersonId = table.Column<int>(type: "INTEGER", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Followings", x => new { x.FollowedPersonId, x.FollowerId });
+                    table.ForeignKey(
+                        name: "FK_Followings_Persons_FollowedPersonId",
+                        column: x => x.FollowedPersonId,
+                        principalTable: "Persons",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_Followings_Persons_FollowerId",
+                        column: x => x.FollowerId,
+                        principalTable: "Persons",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Followings_FollowerId_FollowedPersonId",
+                table: "Followings",
+                columns: new[] { "FollowerId", "FollowedPersonId" },
+                unique: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Followings");
+        }
+    }
+}
